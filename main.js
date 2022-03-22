925b03df35fb4cabfb8ec9fa348e7f556ec98cb0
AnusayaPalei <84262617+AnusayaPalei@users.noreply.github.com>
2022-03-22 09:45:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_qualtity:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("captured_image").innerHTML='<img id="image" src="'+data_uri+'">';
+
+    });
+}
+console.log( 'ml5.version', ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/13PwzEwwN/model.json', modelloaded);
+
+function modelloaded(){
+    console.log('modelloaded')
+}
